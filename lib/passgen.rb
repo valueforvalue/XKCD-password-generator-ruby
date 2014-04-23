853cae62d357b6b40d71ff77fb2fd145100334e3
Jeremy Morris <valueforvalue76@gmail.com>
2014-04-22 20:50:22 -0500

    Modified directory structure

--- lib/passgen.rb
+++ lib/passgen.rb
@@ -1,1 +1,31 @@
-#Place Holder
+require 'passgen/wordlist'
+require 'passgen/options'
+require 'passgen/generator'
+require 'passgen/printer'
+
+module Passgen
+	class Runner
+		def initialize(argv)
+			@cmd = Options.new(argv)
+		end
+		
+		def run()
+			wordlist = Wordlist.new(@cmd.options[:wordlist])
+			wordlist::create(@cmd.options[:min], @cmd.options[:max])
+			generator = Generator.new()
+			
+			if @cmd.options[:single]
+				generator::gen_single(wordlist.list)
+			elsif @cmd.options[:acrostic]
+				generator::gen_acrostic(wordlist.list, @cmd.options[:acrostic])
+			elsif @cmd.options[:count]
+				generator::gen_multi(wordlist.list, @cmd.options[:count])
+			else 
+				puts "Command slipped through."
+			end
+			Printer.new(generator.pass)
+		end
+		
+	end
+
+end
