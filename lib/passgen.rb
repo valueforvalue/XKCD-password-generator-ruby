a2205ddf97b9aaf7fae65685ea40d38c9b72a34e
Jeremy Morris <valueforvalue76@gmail.com>
2014-05-13 20:16:35 -0500

    removed more tabs

--- lib/passgen.rb
+++ lib/passgen.rb
@@ -7,43 +7,43 @@ require 'passgen/printer'
 module Passgen
 
     # The runner class sews all other classes together.
-	# It calls the option parser on *ARGV* to procces command line options.
-	class Runner
-	    # Sets up option parsing.
-		def initialize(argv)
-			@opts = Options.new(argv)
-			@cmd = @opts.options
-		end
-		
-		
-		# Driver code for passgen.
-		#
-		# The *run* method takes care of init,
-		# and calls the generator method that is applicable 
-		# based on the command line args.
-		#
-		def run()
-			wordlist = Wordlist.new(@cmd)
-			@cmd[:wordlist] = wordlist
-			generator = Generator.new()
-			
-			if @cmd[:list]
-			  wordlist::print_wordfiles
-			end
-			
-			if @cmd[:generate]
-			  generator::gen_multi(@cmd)
-			elsif @cmd[:acrostic]
-			  generator::gen_acrostic(@cmd)
-			end
-			
-			if generator.pass
-			  @cmd[:password] = generator.pass
-			  Printer.new(@cmd)
-			end
-			
-		end
-		
-	end
+  # It calls the option parser on *ARGV* to procces command line options.
+  class Runner
+      # Sets up option parsing.
+    def initialize(argv)
+      @opts = Options.new(argv)
+      @cmd = @opts.options
+    end
+    
+    
+    # Driver code for passgen.
+    #
+    # The *run* method takes care of init,
+    # and calls the generator method that is applicable 
+    # based on the command line args.
+    #
+    def run()
+      wordlist = Wordlist.new(@cmd)
+      @cmd[:wordlist] = wordlist
+      generator = Generator.new()
+      
+      if @cmd[:list]
+        wordlist::print_wordfiles
+      end
+      
+      if @cmd[:generate]
+        generator::gen_multi(@cmd)
+      elsif @cmd[:acrostic]
+        generator::gen_acrostic(@cmd)
+      end
+      
+      if generator.pass
+        @cmd[:password] = generator.pass
+        Printer.new(@cmd)
+      end
+      
+    end
+    
+  end
 
 end
