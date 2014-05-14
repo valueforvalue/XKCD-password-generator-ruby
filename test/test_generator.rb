631cf06340828822fa1370bc9f7eb53f4f8dba3a
Jeremy Morris <valueforvalue76@gmail.com>
2014-05-13 22:35:10 -0500

    Fixed RuboCop issues in test suite.

--- test/test_generator.rb
+++ test/test_generator.rb
@@ -1,43 +1,41 @@
+﻿# coding: utf-8
 gem 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 require_relative '../lib/passgen/generator'
 require_relative '../lib/passgen/wordlist'
 
-
 class TestGenerator < MiniTest::Test
   def setup
-  
-	@options = { :wordlist => "test.txt",
-            :min => 3,
-            :max => 5,
-            :count => 4,
-			:generate => false,
-			:delim => " ",
-			:number => 1,
-			:filename => "output.txt",
-			:print => false,
-			:acrostic => "past",
-			:valid => ".",
-            }
+    @options = {
+      wordlist: 'test.txt',
+      min: 3,
+      max: 5,
+      count: 4,
+      generate: false,
+      delim: ' ',
+      number: 1,
+      filename: 'output.txt',
+      print: false,
+      acrostic: 'past',
+      valid: '.'
+      }
     @wordlist = Passgen::Wordlist.new(@options)
-	@options[:wordlist] = @wordlist
-	@generator = Passgen::Generator.new
-    @target  = [["party", "and", "sing", "true"]]
+    @options[:wordlist] = @wordlist
+    @generator = Passgen::Generator.new
+    @target  = [%w(party and sing true)]
   end
 
-	
   def test_acrostic_generation
-    assert_equal @target, @generator::gen_acrostic(@options)
+    assert_equal @target, @generator.gen_acrostic(@options)
   end
-  
+
   def test_multi_generation
-    assert_equal 4, @generator::gen_multi(@options)[0].length
+    assert_equal 4, @generator.gen_multi(@options)[0].length
   end
-  
+
   def test_multi_generation_num
-	@options[:number] = 10
-    assert_equal 10, @generator::gen_multi(@options).length
-  end	
-  	
+    @options[:number] = 10
+    assert_equal 10, @generator.gen_multi(@options).length
+  end
 end
--- test/test_wordlist.rb
+++ test/test_wordlist.rb
@@ -1,43 +1,41 @@
+﻿# coding: utf-8
 gem 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 require_relative '../lib/passgen/wordlist'
 
-
-
-	
 class TestWordlist < MiniTest::Test
   def setup
-  
-	@options = { :wordlist => "test.txt",
-            :min => 3,
-            :max => 5,
-            :count => 4,
-			:generate => false,
-			:delim => " ",
-			:number => 1,
-			:filename => "output.txt",
-			:print => false,
-			:acrostic => "past",
-			:valid => ".",
-            }
+    @options = {
+      wordlist: 'test.txt',
+      min: 3,
+      max: 5,
+      count: 4,
+      generate: false,
+      delim: ' ',
+      number: 1,
+      filename: 'output.txt',
+      print: false,
+      acrostic: 'past',
+      valid: '.'
+      }
     @wordlist = Passgen::Wordlist.new(@options)
-	@options[:wordlist] = @wordlist
+    @options[:wordlist] = @wordlist
   end
 
   def test_wordlist_path
-    assert_equal "c:/Users/A PLUS/passgen/lib/passgen/res/test.txt" , @wordlist.path
+    assert_equal 'c:/Users/A PLUS/passgen/lib/passgen/res/test.txt' , @wordlist.path
   end
-  
+
   def test_wordlist_size
-    assert_equal  4 , @options[:wordlist].length
+    assert_equal 4 , @options[:wordlist].length
   end
-  
+
   def test_wordlist_get_word
-    refute_nil @options[:wordlist].get_word()
-  end  
-  
+    refute_nil @options[:wordlist].get_word
+  end
+
   def test_wordlist_print_wordfiles
-    refute_nil @options[:wordlist].print_wordfiles()
+    refute_nil @options[:wordlist].print_wordfiles
   end
 end
