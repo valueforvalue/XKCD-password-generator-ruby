9e2f521690bf0ffd529589878c3d3ccfc82a1c4a
Jeremy Morris <valueforvalue76@gmail.com>
2014-05-13 22:35:10 -0500

    Fixed RuboCop issues in test suite.

--- test/test_options.rb
+++ test/test_options.rb
@@ -1,3 +1,4 @@
+﻿# coding: utf-8
 gem 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
@@ -5,56 +6,57 @@ require_relative '../lib/passgen/options'
 
 
 class TestOptions < MiniTest::Test
-  def setup(extra="")
-	@options = [ "-w","test_wordlist.txt",
-            "-m", "4",
-            "-x", "8",
-            "-c", "5",
-			"-d", "-",
-			"-n", "12",
-			"-p", 
-			"test_output.txt",
-			"-a", "anacrostic",
-			"-v", "[a-z]", "-l",
-            ]
-	@options << extra
-	@opts = Passgen::Options.new(@options)
+  def setup(extra = '')
+    @options = [
+      '-w', 'test_wordlist.txt',
+      '-m', '4',
+      '-x', '8',
+      '-c', '5',
+      '-d', '-',
+      '-n', '12',
+      '-p',
+      'test_output.txt',
+      '-a', 'anacrostic',
+      '-v', '[a-z]', '-l'
+      ]
+    @options << extra
+    @opts = Passgen::Options.new(@options)
   end
-  
+
   def test_options_wordlist_default
-	assert_equal "test_wordlist.txt", @opts.options[:wordlist]
+    assert_equal 'test_wordlist.txt', @opts.options[:wordlist]
   end
-	
+
   def test_options_wordlist
-	assert_equal "test_wordlist.txt", @opts.options[:wordlist]
+    assert_equal 'test_wordlist.txt', @opts.options[:wordlist]
   end
-  
+
   def test_options_acrostic
-	assert_equal "anacrostic" , @opts.options[:acrostic]
+    assert_equal 'anacrostic' , @opts.options[:acrostic]
   end
 
   def test_options_wordlist_acrostic
-	assert_equal "anacrostic" , @opts.options[:acrostic]
-	assert_equal "test_wordlist.txt" , @opts.options[:wordlist]
+    assert_equal 'anacrostic' , @opts.options[:acrostic]
+    assert_equal 'test_wordlist.txt' , @opts.options[:wordlist]
   end
-  
+
   def test_options_options
-	assert_equal 4 , @opts.options[:min]
-	assert_equal 8 , @opts.options[:max]
-	assert_equal 5 , @opts.options[:count]
-	assert_equal "-" , @opts.options[:delim]
-	assert_equal 12 , @opts.options[:number]
-	assert_equal "test_output.txt" , @opts.options[:filename]
-	assert_equal true , @opts.options[:print]
-	assert_equal "anacrostic" , @opts.options[:acrostic]
-	assert_equal "[a-z]" , @opts.options[:valid]
-	assert_equal true , @opts.options[:list]
+    assert_equal 4 , @opts.options[:min]
+    assert_equal 8 , @opts.options[:max]
+    assert_equal 5 , @opts.options[:count]
+    assert_equal '-' , @opts.options[:delim]
+    assert_equal 12 , @opts.options[:number]
+    assert_equal 'test_output.txt' , @opts.options[:filename]
+    assert_equal true , @opts.options[:print]
+    assert_equal 'anacrostic' , @opts.options[:acrostic]
+    assert_equal '[a-z]' , @opts.options[:valid]
+    assert_equal true , @opts.options[:list]
   end
-  
+
   def test_generate_option
     # This must be tested in isolation because the *-g* and *-a* options
-	# cannot be set at the same time.
-    setup("-g")
-	assert_equal true , @opts.options[:generate]
+    # cannot be set at the same time.
+    setup('-g')
+    assert_equal true , @opts.options[:generate]
   end
 end
